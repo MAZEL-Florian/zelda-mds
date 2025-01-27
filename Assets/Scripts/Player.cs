e79657a80b60789862294bb06a7768170dc70a02
MAZEL Florian <mazel.florian1@gmail.com>
2025-01-27 14:13:06 +0100

    feat: add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{    
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody2D;
+    public float moveSpeed = 15f;
+    private Animator _animator;
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        Debug.Log("Start player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        _movement = new Vector2(horizontal, vertical).normalized;
+        
+        _animator.SetFloat("Horizontal", horizontal);
+        _animator.SetFloat("Vertical", vertical);
+        _animator.SetFloat("Velocity", _movement.sqrMagnitude);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.linearVelocity = _movement * moveSpeed;
+    }
+}
